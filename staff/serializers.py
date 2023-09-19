6c811a9401f083b2a7365b874b6cfeffaf1276a2
Ryan Kwok <kwokyto@gmail.com>
2023-09-19 22:02:16 +0800

    Create ticketDetails API

--- staff/serializers.py
+++ staff/serializers.py
@@ -1,7 +1,9 @@
+import json
 from django.utils import timezone
+from django.core.serializers import serialize
 from rest_framework import serializers
 
-from customer.models import Accounts, AccountTypes
+from customer.models import Accounts, AccountTypes, Customer
 from staff.validations import validate_open_ticket, validate_ticket_id
 
 from .models import Staff, Tickets
@@ -12,21 +14,19 @@ class StaffSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Staff
-        fields = ('user', 'first_name', 'last_name', 'title', 'birth_date', 'gender')
+        fields = ("user", "first_name", "last_name", "title", "birth_date", "gender")
 
     def create(self, validated_data):
         return Staff.objects.create(**validated_data)
 
 
 class GetAccountTypesSerializer(serializers.Serializer):
-    
     def get_account_type_list(self):
         all_account_types = AccountTypes.objects.all().values_list("name", flat=True)
         return list(all_account_types)
 
-      
+
 class ApproveSerializer(serializers.Serializer):
-    
     def __init__(self, user_id, json_dict, **kwargs):
         self.user_id = user_id
         self.json_dict = json_dict
@@ -45,15 +45,18 @@ class ApproveSerializer(serializers.Serializer):
         self.make_account()
         self.ticket.save()
         return self.ticket
-    
+
     def make_account(self):
-        account = Accounts.objects.create(user=self.ticket.created_by, type=self.ticket.account_type, status=Accounts.AccountStatus.ACTIVE)
+        account = Accounts.objects.create(
+            user=self.ticket.created_by,
+            type=self.ticket.account_type,
+            status=Accounts.AccountStatus.ACTIVE,
+        )
         account.save()
         return account
 
 
 class RejectSerializer(serializers.Serializer):
-    
     def __init__(self, user_id, json_dict, **kwargs):
         self.user_id = user_id
         self.json_dict = json_dict
@@ -74,18 +77,53 @@ class RejectSerializer(serializers.Serializer):
 
 
 class GetOpenTicketsSerializer(serializers.Serializer):
-    
     def get_open_tickets_list(self):
-        self.all_open_tickets = Tickets.objects.filter(status=Tickets.TicketStatus.OPEN).order_by("created_date").values()
+        self.all_open_tickets = (
+            Tickets.objects.filter(status=Tickets.TicketStatus.OPEN)
+            .order_by("created_date")
+            .values()
+        )
         return list(self.all_open_tickets)
 
 
 class GetClosedTicketsSerializer(serializers.Serializer):
-    
     def __init__(self, user_id):
         self.user_id = user_id
 
     def get_closed_tickets_list(self):
-        staff = Staff.objects.get(user = self.user_id)
-        closed_tickets = Tickets.objects.filter(closed_by=staff).exclude(status=Tickets.TicketStatus.OPEN).order_by("closed_date").reverse().values()
+        staff = Staff.objects.get(user=self.user_id)
+        closed_tickets = (
+            Tickets.objects.filter(closed_by=staff)
+            .exclude(status=Tickets.TicketStatus.OPEN)
+            .order_by("closed_date")
+            .reverse()
+            .values()
+        )
         return list(closed_tickets)
+
+
+class TicketDetailsSerializer(serializers.Serializer):
+    def __init__(self, user_id, json_dict, **kwargs):
+        self.user_id = user_id
+        self.json_dict = json_dict
+        super().__init__(**kwargs)
+
+    def validate(self, attrs):
+        self.ticket = validate_ticket_id(self.json_dict)
+        return super().validate(attrs)
+
+    def get_ticket_details(self):
+        customer = self.ticket.created_by
+        accounts = Accounts.objects.filter(user=customer).values()
+        
+        ticket_json = serialize("json", [self.ticket,])
+        ticket_json = json.loads(ticket_json)[0]["fields"]
+        customer_json = serialize("json", [customer,])
+        customer_json = json.loads(customer_json)[0]["fields"]
+
+        result = {
+            "ticket": ticket_json,
+            "customer": customer_json,
+            "accounts": list(accounts),
+        }
+        return result
