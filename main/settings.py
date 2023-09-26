a82091025c1486704d49ca4baba7d0db3f5f6dc3
darrenangwx <darrenawx@gmail.com>
2023-09-26 18:36:27 +0800

    update settings.py with migration_db settings

--- main/settings.py
+++ main/settings.py
@@ -102,6 +102,24 @@ DATABASES = {
             'NAME': 'test_drck_banking',
         }
     },
+    'migration_db': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'OPTIONS': {
+            'options': '-c search_path=django',
+            'sslmode': 'verify-ca',
+            'sslcert': os.environ["CLIENT_CERT"],
+            'sslkey': os.environ["CLIENT_KEY"],
+            'sslrootcert': os.environ["CA_CERT"],    
+        },
+        'NAME': os.environ["POSTGRES_DBNAME_AUTH"],
+        'USER': os.environ["POSTGRES_USERMIGRATE_AUTH"],
+        'PASSWORD': os.environ["POSTGRES_USERMIGRATE_PASSWORD_AUTH"],
+        'HOST': os.environ["POSTGRES_HOST_AUTH"],
+        'PORT': os.environ["POSTGRES_PORT_AUTH"],
+        'TEST': {
+            'NAME': 'test_drck_banking',
+        }
+    },
 }
 
 
