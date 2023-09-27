541534bed1b155bfc7a5f5312669c93afb111db0
darrenangwx <darrenawx@gmail.com>
2023-09-27 15:10:58 +0800

    update settings.py

--- main/settings.py
+++ main/settings.py
@@ -190,7 +190,8 @@ REST_KNOX = {
 }
 
 # CORS header
-CORS_ALLOWED_ORIGINS = [
-    "https://ifs4205-23s1-1-1-i.comp.nus.edu.sg:8080",
-    "http://192.168.37.141:8080"
-]
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = [
+#     "https://ifs4205-23s1-1-1-i.comp.nus.edu.sg:8080",
+#     "http://192.168.37.141:8080"
+# ]
