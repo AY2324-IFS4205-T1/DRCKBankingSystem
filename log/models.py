ba3275b83284f1ebbf474434124a2268ad4b2c9a
pinyoko573 <pinyoko573@gmail.com>
2023-10-08 03:21:37 +0800

    Created logging models

---
+++ log/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+
+from user.models import User
+from customer.models import Transactions
+
+# Create your models here
+class Severity(models.TextChoices):
+    HIGH = "High"
+    MEDIUM = "Medium"
+    LOW = "Low"
+
+class APIMethod(models.TextChoices):
+    GET = "GET"
+    POST = "POST"
+
+class LoginLog(models.Model):
+    class Meta:
+        db_table = 'log"."login_log'
+
+    id = models.AutoField(primary_key=True)
+    level = models.CharField(max_length=6, choices=Severity.choices)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_success = models.BooleanField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    ip = models.IPAddressField()
+
+class APILog(models.Model):
+    class Meta:
+        db_table = 'log"."api_log'
+
+    id = models.AutoField(primary_key=True)
+    level = models.CharField(max_length=6, choices=Severity.choices)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    api = models.TextField()
+    method = models.CharField(max_length=4, choices=APIMethod.choices)
+    ip = models.IPAddressField()
+
+class TransactionLog(models.Model):
+    class Meta:
+        db_table = 'log"."transaction_log'
+
+    transaction_id = models.OneToOneField(Transactions, primary_key=True, on_delete=models.CASCADE)
+    level = models.CharField(max_length=6, choices=Severity.choices)
+    message = models.TextField()
