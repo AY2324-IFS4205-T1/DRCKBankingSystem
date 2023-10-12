fa48187b1b1b7c326d649cfa69f7cb3a215fe608
Ryan Kwok <kwokyto@gmail.com>
2023-10-13 00:12:21 +0800

    Add tests for logging

--- log/tests.py
+++ log/tests.py
@@ -1,6 +1,8 @@
 
+from django.forms import model_to_dict
 from django.urls import reverse
 from rest_framework import status
+from log.models import AccessControlLogs, LoginLog
 
 from user.tests import TestLogout
 
@@ -8,67 +10,58 @@ good_start = "2023-03-09T00:00"
 good_end = "2023-11-09T00:00"
 
 class TestLoginLogging(TestLogout): # staff action
-    def test_login_logs(self):
-        sample_login_logs = {"severity": "High", "start": good_start, "end": good_end}
-        bad_field_severity = {"ssssseverity": "High", "start": good_start, "end": good_end}
-        bad_field_start = {"severity": "High", "sssstart": good_start, "end": good_end}
-        bad_field_end = {"severity": "High", "start": good_start, "eeeeend": good_end}
-        bad_value_severity = {"severity": "HHHHigh", "start": good_start, "end": good_end}
-        bad_value_start = {"severity": "High", "start": "2023-0ferg00:00", "end": good_end}
-        bad_value_end = {"severity": "High", "start": good_start, "end": "2023-grdnyt0:00"}
-        bad_value_end_after_now = {"severity": "High", "start": good_start, "end": "2050-11-09T00:00"}
-        bad_value_end_before_start = {"severity": "High", "start": "2023-09-09T00:00", "end": good_start}
+    def test_should_log(self):
+        for _ in range(5):
+            login = {"username": "test1", "password": "wrongpassword"}
+            self.client.post(reverse("customerLogin"), login)
+        logs = LoginLog.objects.all()
+        self.assertEqual(len(logs), 5)
 
-        response = self.client.post(reverse("login_logs"), sample_login_logs)
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+    # def test_login_logs(self):
+    #     sample_login_logs = {"severity": "High", "start": good_start, "end": good_end}
+    #     bad_field_severity = {"ssssseverity": "High", "start": good_start, "end": good_end}
+    #     bad_field_start = {"severity": "High", "sssstart": good_start, "end": good_end}
+    #     bad_field_end = {"severity": "High", "start": good_start, "eeeeend": good_end}
+    #     bad_value_severity = {"severity": "HHHHigh", "start": good_start, "end": good_end}
+    #     bad_value_start = {"severity": "High", "start": "2023-0ferg00:00", "end": good_end}
+    #     bad_value_end = {"severity": "High", "start": good_start, "end": "2023-grdnyt0:00"}
+    #     bad_value_end_after_now = {"severity": "High", "start": good_start, "end": "2050-11-09T00:00"}
+    #     bad_value_end_before_start = {"severity": "High", "start": "2023-09-09T00:00", "end": good_start}
 
+    #     response = self.client.post(reverse("login_logs"), sample_login_logs)
+    #     self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    #     self.two_fa_customer_1()
+    #     response = self.client.post(reverse("login_logs"), sample_login_logs, **self.header)
+    #     self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    #     self.two_fa_staff1()
+    #     response = self.client.post(reverse("login_logs"), sample_login_logs, **self.header)
+    #     self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    #     self.two_fa_staff3()
+    #     response = self.client.post(reverse("login_logs"), bad_value_end_before_start, **self.header)
+    #     self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        
+    #     requests = [sample_login_logs, bad_field_severity, bad_field_start, bad_field_end, bad_value_severity, bad_value_start, bad_value_end, bad_value_end_after_now]
+    #     for request in requests:
+    #         response = self.client.post(reverse("login_logs"), request, **self.header)
+    #         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+
+class TestAccessControlLogging(TestLogout): # staff action
+    def test_should_log(self):
         self.two_fa_customer_1()
-        response = self.client.post(reverse("login_logs"), sample_login_logs, **self.header)
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.client.get(reverse("staffWelcome"), **self.header)
 
         self.two_fa_staff1()
-        response = self.client.post(reverse("login_logs"), sample_login_logs, **self.header)
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.client.get(reverse("customerWelcome"), **self.header)
+        self.client.get(reverse("anonymisation"), **self.header)
+        self.client.get(reverse("login_logs"), **self.header)
 
         self.two_fa_staff3()
-        response = self.client.post(reverse("login_logs"), bad_value_end_before_start, **self.header)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        
-        requests = [sample_login_logs, bad_field_severity, bad_field_start, bad_field_end, bad_value_severity, bad_value_start, bad_value_end, bad_value_end_after_now]
-        for request in requests:
-            response = self.client.post(reverse("login_logs"), request, **self.header)
-            self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.client.get(reverse("getOpenTickets"), **self.header)
 
-
-class TestAPILogging(TestLogout): # staff action
-    def test_API_logs(self):
-        sample_login_logs = {"severity": "High", "start": good_start, "end": good_end}
-        bad_field_severity = {"ssssseverity": "High", "start": good_start, "end": good_end}
-        bad_field_start = {"severity": "High", "sssstart": good_start, "end": good_end}
-        bad_field_end = {"severity": "High", "start": good_start, "eeeeend": good_end}
-        bad_value_severity = {"severity": "HHHHigh", "start": good_start, "end": good_end}
-        bad_value_start = {"severity": "High", "start": "2023-0ferg00:00", "end": good_end}
-        bad_value_end = {"severity": "High", "start": good_start, "end": "2023-grdnyt0:00"}
-        bad_value_end_after_now = {"severity": "High", "start": good_start, "end": "2050-11-09T00:00"}
-        bad_value_end_before_start = {"severity": "High", "start": "2023-09-09T00:00", "end": good_start}
-
-        response = self.client.post(reverse("api_logs"), sample_login_logs)
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-
-        self.two_fa_customer_1()
-        response = self.client.post(reverse("api_logs"), sample_login_logs, **self.header)
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
-        self.two_fa_staff1()
-        response = self.client.post(reverse("api_logs"), sample_login_logs, **self.header)
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
-        self.two_fa_staff3()
-        response = self.client.post(reverse("api_logs"), bad_value_end_before_start, **self.header)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        
-        requests = [sample_login_logs, bad_field_severity, bad_field_start, bad_field_end, bad_value_severity, bad_value_start, bad_value_end, bad_value_end_after_now]
-        for request in requests:
-            response = self.client.post(reverse("api_logs"), request, **self.header)
-            self.assertEqual(response.status_code, status.HTTP_200_OK)
-
+        logs = AccessControlLogs.objects.all()
+        self.assertEqual(len(logs), 5)
