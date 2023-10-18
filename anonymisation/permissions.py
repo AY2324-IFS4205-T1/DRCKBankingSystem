829a176a5c0eec8af54a1782760608b3d17d0853
Ryan Kwok <kwokyto@gmail.com>
2023-10-18 22:54:47 +0800

    Add logging for permissions

--- anonymisation/permissions.py
+++ anonymisation/permissions.py
@@ -1,7 +1,7 @@
 from rest_framework.permissions import BasePermission
+from log.logging import AccessControlLogger
 
 from staff.models import Staff
-from user.models import User
 
 
 class IsResearcher(BasePermission):
@@ -10,8 +10,11 @@ class IsResearcher(BasePermission):
         """
         Return `True` if permission is granted, `False` otherwise.
         """
-        user = Staff.objects.get(user=request.user)
-        return user.title == Staff.Title.RESEARCHER
+        staff = Staff.objects.get(user=request.user)
+        permission_granted = staff.title == Staff.Title.RESEARCHER
+        if not permission_granted:
+            AccessControlLogger(request, Staff.Title.RESEARCHER, view.get_view_name())
+        return permission_granted
     
 class IsAnonymiser(BasePermission):
     
@@ -19,5 +22,8 @@ class IsAnonymiser(BasePermission):
         """
         Return `True` if permission is granted, `False` otherwise.
         """
-        user = Staff.objects.get(user=request.user)
-        return user.title == Staff.Title.ANONYMISER
+        staff = Staff.objects.get(user=request.user)
+        permission_granted = staff.title == Staff.Title.ANONYMISER
+        if not permission_granted:
+            AccessControlLogger(request, Staff.Title.ANONYMISER, view.get_view_name())
+        return permission_granted
