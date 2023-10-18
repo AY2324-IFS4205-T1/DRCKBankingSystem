2e4795d534d9ec8fe5cf5b08283b4a899d2ecd08
rebecca <e0560032@u.nus.edu>
2023-10-18 22:14:07 +0800

    added database.py

--- anonymisation/anonymise/utils/database.py
+++ anonymisation/anonymise/utils/database.py
@@ -1,11 +1,11 @@
-from anonymisation.models import Anonymisation
+from anonymisation.models import Anonymisation, Statistics
 from django.db import connections
 
 def reset_sequence():
     with connections['default'].cursor() as cursor:
         cursor.execute("ALTER SEQUENCE anonymisation.anon_id_seq RESTART;")
 
-def save_to_database(anon_data):
+def store_anon_database(anon_data):
     Anonymisation.objects.all().delete()
     reset_sequence()
     for data in anon_data:
@@ -24,3 +24,20 @@ def save_to_database(anon_data):
             third_balance=data['third_balance']
         )
         anon_instance.save()
+
+def store_stats_database(k_value, info_loss, first_list, second_list, first_utility, second_utility):
+    anon_instance = Statistics(
+        k_value=k_value,
+        utility_query1=first_utility,
+        utility_query2=second_utility,
+        info_loss=info_loss,
+        first_average=first_list[0],
+        second_average=first_list[1],
+        third_average=first_list[2],
+        fourth_average=first_list[3],
+        fifth_average=first_list[4],
+        first_balance_average=second_list[0],
+        second_balance_average=second_list[1],
+        third_balance_average=second_list[2]
+    )
+    anon_instance.save()
